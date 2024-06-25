0355b8b4a30a5c09d1dea0f4d52a78c437f0192e
Ayush Kumar <ayush4449kumar@gmail.com>
2024-06-25 21:15:33 +0530

    added hover animation

---
+++ public/script.js
@@ -0,0 +1,28 @@
+let ash=document.getElementById("ash_png")
+let burn=document.getElementById("burn_png")
+ash.addEventListener('mouseover',()=>{
+    console.log("mouse enter")
+    tl=gsap.timeline()
+    
+    // gsap.to("#flames",{
+    //     opacity:0.4,
+    //     duration:1.8
+    // })
+    gsap.to("#ash_png",{
+        opacity:0,
+        duration:1.8
+    })
+})
+ash.addEventListener('mouseleave',()=>{
+    console.log("mouse leave")
+    tl=gsap.timeline()
+    gsap.to("#ash_png",{
+        opacity:1,
+        duration:1.8
+    })
+    // gsap.to("#flames",{
+    //     opacity:0,
+    //     duration:1.8
+    // })
+    
+})
