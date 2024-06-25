4ec62b8242b1b48b12e1e7d8bfad9e8a0f6bc025
Ayush Kumar <ayush4449kumar@gmail.com>
2024-06-25 22:02:17 +0530

    page2 faded animation

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 let ash=document.getElementById("ash_png")
 let burn=document.getElementById("burn_png")
+let second_page=document.getElementsByClassName("page2")[0]
 ash.addEventListener('mouseover',()=>{
     console.log("mouse enter")
     tl=gsap.timeline()
@@ -25,4 +26,14 @@ ash.addEventListener('mouseleave',()=>{
     //     duration:1.8
     // })
     
+})
+gsap.from(".page2",{
+    y:350,
+    opacity:0.1,
+    scrollTrigger:{
+        trigger:".page2",
+        start: "top 65%",
+        end: "top 30%",
+        scrub:2
+    }
 })
