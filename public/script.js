978ac491b4a22667a773a58efd3b6d98518822a0
Ayush Kumar <ayush4449kumar@gmail.com>
2024-06-27 17:34:39 +0530

    updated loop animation

--- public/script.js
+++ public/script.js
@@ -3,45 +3,6 @@ let burn=document.getElementById("burn_png")
 let second_page=document.getElementsByClassName("page2")[0]
 let tl = gsap.timeline({});
 
-
-// const button = document.getElementsByClassName('circle')[0];
-// const textbase = document.getElementsByClassName('text-base')[0];
-// // Get the button's bounding rectangle dimensions and position
-// let boundingRect = button.getBoundingClientRect();
-
-// // Add an event listener to the window to update the bounding rectangle dimensions
-// // when the window is resized
-// window.addEventListener('resize', () => {
-//   boundingRect = button.getBoundingClientRect();
-// });
-// console.log(boundingRect);
-// // Add an event listener for mouse movement over the button
-// textbase.addEventListener('mouseover', (e) => {
-//   // Calculate mouse position relative to the button
-//   const mousePosX = e.x - boundingRect.left; // X position
-//   const mousePosY = e.y - boundingRect.top;  // Y position
-//   console.log("enter");
-//   // Use GSAP (GreenSock Animation Platform) to animate the button
-//   // This creates a 'magnetic' effect where the button moves towards the mouse
-//   gsap.to(button, {
-//     x: (mousePosX - boundingRect.width / 2) * 0.4, // Move horizontally towards mouse
-//     y: (mousePosY - boundingRect.height / 2) * 0.4, // Move vertically towards mouse
-//     duration: 0.8, // Duration of the animation
-//     ease: 'power3.out', // Easing function for smooth animation
-//   });
-// });
-
-// // Add an event listener for when the mouse leaves the button area
-// button.addEventListener('mouseleave', () => {
-//   // Animate the button back to its original position
-//   gsap.to(button, {
-//     x: 0, // Reset horizontal position
-//     y: 0, // Reset vertical position
-//     duration: 0.8, // Duration of the animation
-//     ease: 'elastic.out(1,0.3)' // Easing function for a 'springy' return
-//   });
-// });
-
 ash.addEventListener('mouseover',()=>{
     console.log("mouse enter")
     
@@ -93,15 +54,17 @@ tl.from(".text-center",{
     scrollTrigger:{
         trigger:".page3",
         start: "top 65%",
+        
         end: "top 10%",
         scrub:2
         
     }
 })
 tl.from(".flex-text",{
-    y:250,
-    // scale:0.9,
+    y:150,
+    
     opacity:0,
+    // duration:2.2,
     scrollTrigger:{
         trigger:".page3",
         start: "top 65%",
